68c1a482a911a1c229defc3e10ad362d92a6033c
Bhupinder Sehjal <101923266+BhupinderSehjal@users.noreply.github.com>
2024-10-26 11:09:27 +0530

    Add files via upload

---
+++ for-of.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log("for-of loop = It is exclusively used for arrays. \n it is iterate through all the element of an array");
+
+let modules = ("HTML","CSS","PHP","JAVASCRIPT");
+for (let element of modules){
+    console.log(element + " ");
+}
