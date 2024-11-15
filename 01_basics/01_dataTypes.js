b1e01bab3cb853975ea0969a7c7cb8a3acf41235
Bhupinder Sehjal <101923266+BhupinderSehjal@users.noreply.github.com>
2024-11-15 19:30:41 +0000

    dataTypes in JS

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"    //treat all vs codeas newer version
+
+//alert(3+3)  //we are using node js , not browser
+
+console.log("Bhupinder");
+
+let name ="Bhupinder Sehjal"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/* Data types
+number   => 2 to power 53
+//bigInt
+//String
+//boolean => true/false
+//null => standalone
+//undefined =>
+//symbol =>unique
+
+//object
+*/
+
+console.log(typeof "Bhupinder")
+
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+
+
+
+console.log(typeof undefined) //undefined
+
+console.log(typeof null) //object .. Because null is considered as object in javascript language
+
