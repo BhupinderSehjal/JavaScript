016914e1439f015b2a7192b1992a22327a5d96b9
Bhupinder Sehjal <101923266+BhupinderSehjal@users.noreply.github.com>
2024-11-15 18:09:13 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "bhupinder@google.com"
+var accountPassword = "12345678"
+accountCity = "Jaipur"
+let accountState ;
+
+//accountId=2 //not alllowed
+
+accountEmail= "hc@hc.com"
+accountPassword ="21212121"
+accountCity = "Jalandhar"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
